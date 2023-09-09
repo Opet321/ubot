2456c954b2443a62416f3886354ecd9cb6164e8a
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 15:59:20 +0700

    Create __main__.py

---
+++ Ryuzaki/__main__.py
@@ -0,0 +1,21 @@
+import asyncio
+import importlib
+from pyrogram import Client, idle
+from Ryuzaki.modules import ALL_MODULES
+from Ryuzaki import clients
+
+async def start_bot():
+    for all_module in ALL_MODULES:
+        importlib.import_module("Ryuzaki.modules" + all_module)
+        print(f"Successfully Imported {all_module}")
+    for pro in clients:
+        try:
+            await pro.start()
+            ur = await pro.get_me()
+            print(f"Started {ur.first_name}")
+        except Exception as e:
+            print(f"{e}")
+    await idle()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
