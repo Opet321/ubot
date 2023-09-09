79a0f130ee1bcaed843b552c78d9cf73b1afb31b
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 16:33:04 +0700

    Create chatgpt.py

---
+++ Ryuzaki/modules/chatgpt.py
@@ -0,0 +1,29 @@
+# code by @xtdevs
+# library RyuzakiLib
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from Ryuzaki.modules.help import *
+
+from RyuzakiLib.hackertools.chatgpt import RendyDevChat
+
+@Client.on_message(filters.command(["ai", "ask"]) & filters.me)
+async def chatgpt_support(client: Client, message: Message):
+    pro = await message.reply_text("`Processing.....`")
+    deny_ask_hehe = message.text.split(" ", 1)[1] if len(message.command) > 1 else None
+    if not deny_ask_hehe:
+        await pro.edit_text("Give ask from chatgpt")
+        return
+    try:
+        hacking = RendyDevChat(deny_ask_hehe).get_response(message)
+        await client.send_message(
+            message.chat.id,
+            hacking,
+            reply_to_message_id=message.id
+        )
+        await pro.delete()
+    except Exception as e:
+        await pro.edit_text(str(e))
+        return
+    
