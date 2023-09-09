6368d05da0f0618a86626f88e53922c3c4390bf7
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 15:51:16 +0700

    Create help.py

---
+++ Ryuzaki/modules/help.py
@@ -0,0 +1,69 @@
+import asyncio
+
+from prettytable import PrettyTable
+from pyrogram import Client, enums, filters
+from pyrogram.types import Message
+
+from Ryuzaki import CMD_HELP
+from Ryuzaki.tools_all import *
+from config import *
+
+async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
+    xyz = (
+        message.edit_text
+        if bool(message.from_user and message.from_user.is_self or message.outgoing)
+        else (message.reply_to_message or message).reply_text
+    )
+    return await xyz(*args, **kwargs)
+
+@Client.on_message(filters.command(["help"], PREFIXES_CMD) & filters.me)
+async def module_helper(client: Client, message: Message):
+    cmd = message.command
+    help_arg = ""
+    if len(cmd) > 1:
+        help_arg = " ".join(cmd[1:])
+    elif message.reply_to_message and len(cmd) == 1:
+        help_arg = message.reply_to_message.text
+    elif not message.reply_to_message and len(cmd) == 1:
+        ac = PrettyTable()
+        ac.header = False
+        ac.title = "Ryuzaki Ubot Plugins"
+        ac.align = "l"
+        for x in split_list(sorted(CMD_HELP.keys()), 2):
+            ac.add_row([x[0], x[1] if len(x) >= 2 else None])
+        await edit_or_reply(
+            message, f"```{str(ac)}```\n• @RendyProjects"
+        )
+        await message.reply(
+            f"**Usage**:`.help type` **To View Module details**"
+        )
+
+    if help_arg:
+        if help_arg in CMD_HELP:
+            commands: dict = CMD_HELP[help_arg]
+            this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
+            for x in commands:
+                this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
+            this_command += "© @RendyProjects"
+            await edit_or_reply(
+                message, this_command, parse_mode=enums.ParseMode.MARKDOWN
+            )
+        else:
+            await edit_or_reply(
+                message,
+                f"`{help_arg}` **Not a Valid Module Name.**",
+            )
+
+
+def add_command_help(module_name, commands):
+    if module_name in CMD_HELP.keys():
+        command_dict = CMD_HELP[module_name]
+    else:
+        command_dict = {}
+
+    for x in commands:
+        for y in x:
+            if y is not x:
+                command_dict[x[0]] = x[1]
+
+    CMD_HELP[module_name] = command_dict
