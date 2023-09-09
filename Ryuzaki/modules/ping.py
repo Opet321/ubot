26c0f77fcdcdfcd1f396d50a9c326655faa518a4
xtsea <90479255+xtsea@users.noreply.github.com>
2023-09-09 16:41:29 +0700

    Create ping.py

---
+++ Ryuzaki/modules/ping.py
@@ -0,0 +1,44 @@
+import time
+import asyncio
+import os
+from datetime import datetime as dt
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import *
+
+async def get_readable_time(seconds: int) -> str:
+    count = 0
+    up_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "d"]
+
+    while count < 4:
+        count += 1
+        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        up_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    up_time += ":".join(time_list)
+
+    return up_time
+
+@Client.on_message(filters.command("ping", PREFIXES_CMD) & filters.me)
+async def custom_ping_handler(client: Client, message: Message):
+    uptime = await get_readable_time((time.time() - time.time()))
+    start = dt.now()
+    lol = await message.reply_text("**Pong!!**")
+    await asyncio.sleep(1.5)
+    end = dt.now()
+    duration = (end - start).microseconds / 1000
+    await lol.edit_text(
+        f" **Pong !!** " f"`%sms` \n" f" **Uptime** - " f"`{uptime}` " % (duration)
+    )
